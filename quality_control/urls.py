6b081b41681e79ae29208ab76ff35e4e41f816aa
Nikolay Milenko <nikolay.milenko@gmail.com>
2024-04-18 15:32:24 +0300

    Add view classes

--- quality_control/urls.py
+++ quality_control/urls.py
@@ -4,9 +4,9 @@ from quality_control import views
 app_name = 'quality_control'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('bugs/', views.bug_list, name='bug_list'),
     path('features/', views.feature_list, name='feature_list'),
-    path('bugs/<int:bug_id>', views.bug_detail, name='bug_detail'),
-    path('features/<int:feature_id>', views.feature_detail, name='feature_detail'),
+    path('bugs/<int:bug_id>/', views.BugDetailView.as_view(), name='bug_detail'),
+    path('features/<int:feature_id>/', views.FeatureDetailView.as_view(), name='feature_detail'),
 ]
